e974e8edcbbd9ac5ed39d703f9c7f4d55eef1e67
iann.braga <bragaprestes@gmail.com>
2024-01-31 18:25:10 -0300

    Ajuste da rota

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  get 'documentos', to: 'documentos#index'
+  get 'index', to: 'documentos#index'
   post '/enviar_arquivo', to: 'documentos#enviar_arquivo'
 
   root 'documentos#index'
